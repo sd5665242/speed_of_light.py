bc5c708a27e92d1f05ac4397b5eb237b3689afce
sd5665242 <129648271+sd5665242@users.noreply.github.com>
2023-04-18 13:41:45 +0300

    Create speed_of_light.py

---
+++ speed_of_light.py
@@ -0,0 +1,7 @@
+distance = float(input("Введите расстояние (в км): "))
+
+speed_of_light = 299792.458  # км/с
+
+time = distance / speed_of_light
+
+print("Время, которое потребуется свету, чтобы пройти заданное расстояние:", time, "секунд")
